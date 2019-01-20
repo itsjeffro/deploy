072d99464326b005644319a137f1f5a6e3e16904
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-01-20 21:15:34 +1100

    Added HooksTable component

---
+++ resources/assets/js/pages/ProjectActionPage/HooksTable.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import Button from '../../components/Button';
+
+const HooksTable = props => {
+	const {
+		hooks,
+		onHandleEditClick,
+		onHandleRemoveClick
+	} = props;
+
+	return (
+		<table className="table">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>&nbsp;</th>
+				</tr>
+			</thead>
+			<tbody>
+				{hooks.map(hook =>
+					<tr key={hook.id}>
+						<td>
+							{hook.name}
+						</td>
+						<td className="text-right">
+							<Button onClick={() => onHandleEditClick(hook)}>Edit</Button>{' '}
+							<Button onClick={() => onHandleRemoveClick(hook)}>Remove</Button>
+						</td>
+					</tr>
+				)}
+			</tbody>
+		</table>
+	)
+};
+
+export default HooksTable;
