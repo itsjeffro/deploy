126b54f9d4399f09848faf9df0a3627a572b85f3
Jeffery Nielsen <jefftoddnielsen@gmail.com>
2019-01-24 21:06:27 +1100

    Corrected indentation

--- resources/assets/js/pages/DashboardPage/DashboardPage.js
+++ resources/assets/js/pages/DashboardPage/DashboardPage.js
@@ -18,195 +18,205 @@ import TextField from '../../components/TextField';
 import Button from '../../components/Button';
 
 class DashboardPage extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            isFetching: true,
-            projects: [],
-            grantedProviders: [],
-            errors: [],
-            input: {}
-        }
-
-        this.handleInputClick = this.handleInputClick.bind(this);
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleDismissModal = this.handleDismissModal.bind(this);
+    this.state = {
+      isFetching: true,
+      projects: [],
+      grantedProviders: [],
+      errors: [],
+      input: {}
     }
 
-    componentWillMount() {
-        let projectService = new ProjectService;
+    this.handleInputClick = this.handleInputClick.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleDismissModalClick = this.handleDismissModalClick.bind(this);
+  }
 
-        projectService
-            .index()
-            .then(response => {
-                let projects = response.data;
+  componentWillMount() {
+    let projectService = new ProjectService;
 
-                this.setState({
-                    isFetching: false,
-                    projects: projects
-                });
-            });
+    projectService
+      .index()
+      .then(response => {
+        let projects = response.data;
 
-        let accountProviderService = new AccountProviderService;
+        this.setState({
+          isFetching: false,
+          projects: projects
+        });
+      });
 
-        accountProviderService
-          .index('/api/account-providers')
-          .then(response => {
-              let providers = response.data.filter(provider => {
-                return provider.deploy_access_token;
-              });
+    let accountProviderService = new AccountProviderService;
+
+    accountProviderService
+      .index('/api/account-providers')
+      .then(response => {
+        let providers = response.data.filter(provider => {
+          return provider.deploy_access_token;
+        });
   
-              this.setState({grantedProviders: providers});
-          });
-    }
+        this.setState({grantedProviders: providers});
+      });
+  }
 
-    handleInputChange(event) {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
+  /**
+   * Handle input change from the project add form.
+   *
+   * @param {object} event
+   */
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
 
-        let input = Object.assign({}, this.state.input);
-        input[name] = value;
+    let input = Object.assign({}, this.state.input);
+    input[name] = value;
 
-        this.setState({
-            input: input
-        });
-    }
+    this.setState({
+      input: input
+    });
+  }
 
-    handleInputClick(event) {
-        event.preventDefault();
+  /**
+   * Handle click for submitting the create project form.
+   *
+   * @param {object} event
+   */
+  handleInputClick(event) {
+    event.preventDefault();
         
-        const { dispatch } = this.props;
+    const { dispatch } = this.props;
 
-        let projectService = new ProjectService;
+    let projectService = new ProjectService;
 
-        projectService
-            .post(this.state.input)
-            .then(response => {
-                dispatch(alertShow('Project created successfully.'));
-
-                $('#project-create-modal').modal('hide');
-
-                let projects = this.state.projects.concat(response.data);
-
-                this.setState({
-                    projects: projects,
-                    errors: []
-                });
-            },
-            error => {
-                let errorResponse = error.response.data;
-
-                errorResponse = errorResponse.hasOwnProperty('errors') ? errorResponse.errors : errorResponse;
-            	
-                const errors = Object.keys(errorResponse).reduce(function(previous, key) {
-                    return previous.concat(errorResponse[key][0]);
-                }, []);
-
-                this.setState({
-                    errors: errors
-                });
-            });
-    }
-
-    handleDismissModal(event) {
-        this.setState({
-            errors: []
-        });
+    projectService
+      .post(this.state.input)
+      .then(response => {
+        dispatch(alertShow('Project created successfully.'));
 
         $('#project-create-modal').modal('hide');
+
+        let projects = this.state.projects.concat(response.data);
+
+        this.setState({
+          projects: projects,
+          errors: []
+        });
+      },
+      error => {
+        let errorResponse = error.response.data;
+
+        errorResponse = errorResponse.hasOwnProperty('errors') ? errorResponse.errors : errorResponse;
+            	
+        const errors = Object.keys(errorResponse).reduce(function(previous, key) {
+          return previous.concat(errorResponse[key][0]);
+        }, []);
+
+        this.setState({errors: errors});
+      });
+  }
+
+  /**
+   * Handle click for dismissing the creat project modal.
+   *
+   * @param {object} event
+   */
+  handleDismissModalClick(event) {
+    this.setState({errors: []});
+
+    $('#project-create-modal').modal('hide');
+  }
+
+  render() {
+    const {errors} = this.state;
+
+    let projectContent = <div className="panel-body">Loading ...</div>;
+
+    if (!this.state.isFetching && this.state.projects.length === 0) {
+      projectContent = <div className="panel-body">Add a project to get started!</div>;
     }
 
-    render() {
-        const {
-            errors
-        } = this.state;
+    if (!this.state.isFetching && this.state.projects.length > 0) {
+      projectContent = <div className="table-responsive"><ProjectsTable projects={this.state.projects} /></div>
+    }
 
-        let projectContent = <div className="panel-body">Loading ...</div>;
-
-        if (!this.state.isFetching && this.state.projects.length === 0) {
-            projectContent = <div className="panel-body">Add a project to get started!</div>;
-        }
-
-        if (!this.state.isFetching && this.state.projects.length > 0) {
-            projectContent = <div className="table-responsive"><ProjectsTable projects={this.state.projects} /></div>
-        }
-
-        return (
-            <div>
-                <div className="breadcrumbs">
-                    <div className="container">
-                        <div className="pull-left">
-                            <span className="heading">Project List</span>
-                        </div>
-                        <div className="pull-right">
-                            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#project-create-modal">
-                                <Icon iconName="plus" /> Add Project
-                            </button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="container content">
-                    <div className="panel panel-default">
-                        {projectContent}
-                    </div>
-                </div>
-
-                <Dialog id="project-create-modal">
-                    <DialogTitle showCloseButton={true}>
-                        Add Project
-                    </DialogTitle>
-                    <DialogContent>
-                        {errors.length ? <AlertErrorValidation errors={errors} /> : ''}
-
-                        <h4>Project Details</h4>
-                        <div className="form-group">
-                            <TextField 
-                                id="name" 
-                                label="Project Name" 
-                                onChange={this.handleInputChange} 
-                                name="name"
-                            />
-                        </div>
-
-                        <h4>Source Control</h4>
-                        <div className="form-group">
-                            <label>Providers</label>
-
-                            {this.state.grantedProviders.map(grantedProvider => (
-                                <div key={grantedProvider.id}>
-                                    <label htmlFor={grantedProvider.name}>
-                                        <input name="provider_id"
-                                            type="radio"
-                                            value={grantedProvider.id}
-                                            id={grantedProvider.name}
-                                            onChange={this.handleInputChange}
-                                        /> {grantedProvider.name}
-                                    </label>
-                                </div>
-                            ))}
-                        </div>
-
-                        <div className="form-group">
-                            <TextField 
-                                id="repository" 
-                                label="Respository" 
-                                onChange={this.handleInputChange} 
-                                name="repository"
-                                placeholder="user/repository"
-                            />
-                        </div>
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={this.handleDismissModal}>Cancel</Button>
-                        <Button color="primary" onClick={this.handleInputClick}>Add Project</Button>
-                    </DialogActions>
-                </Dialog>
+    return (
+      <div>
+        <div className="breadcrumbs">
+          <div className="container">
+            <div className="pull-left">
+              <span className="heading">Project List</span>
             </div>
-        )
-    }
+            <div className="pull-right">
+              <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#project-create-modal">
+                <Icon iconName="plus" /> Add Project
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="container content">
+          <div className="panel panel-default">
+            {projectContent}
+          </div>
+        </div>
+
+        <Dialog id="project-create-modal">
+          <DialogTitle showCloseButton>
+            Add Project
+          </DialogTitle>
+          <DialogContent>
+            {errors.length ? <AlertErrorValidation errors={errors} /> : ''}
+
+            <h4>Project Details</h4>
+            <div className="form-group">
+              <TextField 
+                id="name" 
+                label="Project Name" 
+                onChange={this.handleInputChange} 
+                name="name"
+              />
+            </div>
+
+            <h4>Source Control</h4>
+            <div className="form-group">
+              <label>Providers</label>
+
+              {this.state.grantedProviders.map(grantedProvider => (
+                <div key={grantedProvider.id}>
+                  <label htmlFor={grantedProvider.name}>
+                    <input 
+                      name="provider_id"
+                      type="radio"
+                      value={grantedProvider.id}
+                      id={grantedProvider.name}
+                      onChange={this.handleInputChange}
+                    /> {grantedProvider.name}
+                  </label>
+                </div>
+              ))}
+            </div>
+
+            <div className="form-group">
+              <TextField 
+                id="repository" 
+                label="Respository" 
+                onChange={this.handleInputChange} 
+                name="repository"
+                placeholder="user/repository"
+              />
+            </div>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleDismissModalClick}>Cancel</Button>
+            <Button color="primary" onClick={this.handleInputClick}>Add Project</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    )
+  }
 }
 
 export default connect()(DashboardPage);
