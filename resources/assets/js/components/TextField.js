0be26a3f08920b211134d020cece5c9e703e9913
Jeff Nielsen <jeff.nielsen@4mation.com.au>
2019-02-01 08:32:56 +1100

    Added form-group className

--- resources/assets/js/components/TextField.js
+++ resources/assets/js/components/TextField.js
@@ -12,7 +12,7 @@ const TextField = props => {
     } = props;
 
     return (
-        <div>
+        <div className="form-group">
             {label ? <label htmlFor={id}>{label}</label> : ''}
             <input 
                 className="form-control" 
