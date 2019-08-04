300bb3e3c8e00d16131b6df9fbe3b9c68c4d776f
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-08-04 10:15:15 +1000

    Created component AddProjectDialog

---
+++ resources/assets/js/pages/DashboardPage/AddProjectDialog.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+import AlertErrorValidation from '../../components/AlertErrorValidation';
+import Button from '../../components/Button';
+import Dialog from '../../components/Dialog';
+import DialogActions from '../../components/DialogActions';
+import DialogContent from '../../components/DialogContent';
+import DialogTitle from '../../components/DialogTitle';
+import TextField from '../../components/TextField';
+
+const AddProjectDialog = props => {
+  const {
+    grantedProviders,
+    handleCreateProjectClick,
+    handleDismissModalClick,
+    handleInputChange,
+    projects,
+  } = props;
+
+  return (
+    <Dialog id="project-create-modal">
+      <DialogTitle>
+        Add Project
+      </DialogTitle>
+      <DialogContent>
+        {projects.errors.length ? <AlertErrorValidation errors={projects.errors} /> : ''}
+
+        <h4>Project Details</h4>
+        <div className="form-group">
+          <TextField 
+            id="name" 
+            label="Project Name" 
+            onChange={e => handleInputChange(e)} 
+            name="name"
+          />
+        </div>
+
+        <h4>Source Control</h4>
+        <div className="form-group">
+          <label>Providers</label>
+
+          {grantedProviders.map(grantedProvider => (
+            <div key={grantedProvider.id}>
+              <label htmlFor={grantedProvider.name}>
+                <input 
+                  name="provider_id"
+                  type="radio"
+                  value={grantedProvider.id}
+                  id={grantedProvider.name}
+                  onChange={e => handleInputChange(e)}
+                /> {grantedProvider.name}
+              </label>
+            </div>
+          ))}
+        </div>
+
+        <div className="form-group">
+          <TextField 
+            id="repository" 
+            label="Respository" 
+            onChange={e => handleInputChange(e)} 
+            name="repository"
+            placeholder="user/repository"
+          />
+        </div>
+      </DialogContent>
+      <DialogActions>
+        <Button
+          onClick={e => handleDismissModalClick()}
+        >Cancel</Button>
+
+        <Button
+          color="primary"
+          onClick={e => handleCreateProjectClick()}
+        >{projects.isCreating ? 'Working...' : 'Add Project'}</Button>
+      </DialogActions>
+    </Dialog>
+  )
+};
+
+export default AddProjectDialog;
