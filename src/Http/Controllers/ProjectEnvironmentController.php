8714463912e0253078c6fe60ff4d7a04e472817f
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-08-18 11:11:26 +1000

    Check servers belong to project and sync servers

--- src/Http/Controllers/ProjectEnvironmentController.php
+++ src/Http/Controllers/ProjectEnvironmentController.php
@@ -7,15 +7,17 @@ use Deploy\Jobs\WriteEnvironmemtJob;
 use Deploy\Models\Project;
 use Deploy\Models\Environment;
 use Deploy\Environment\EnvironmentEncrypter;
+use Deploy\Models\Server;
 
 class ProjectEnvironmentController extends Controller
 {
     /**
      * Update environment.
      *
-     * @param  \Deploy\Http\Requests\EnvironmentRequest $request
-     * @param  \Deploy\Models\Project $project
+     * @param EnvironmentRequest $request
+     * @param Project $project
      * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
      */
     public function update(EnvironmentRequest $request, Project $project)
     {
@@ -26,11 +28,25 @@ class ProjectEnvironmentController extends Controller
         $encrypter = new EnvironmentEncrypter($request->get('key'));
 
         if (!$environment) {
-            return response()->json('Environment does not exist for project.', 404);
+            return response()->json(
+                sprintf('Environment does not exist for project with id %i', $project->id),
+                404
+            );
         }
 
         $environment->contents = $encrypter->encrypt($request->get('contents'));
         $environment->save();
+
+        // Sync servers to environment
+        $servers = Server::where('project_id', $project->id)
+            ->whereIn('id', $request->get('servers'))
+            ->get()
+            ->pluck('id')
+            ->toArray();
+
+        if (is_array($servers)) {
+            $environment->environmentServers()->sync($servers);
+        }
 
         dispatch(new WriteEnvironmemtJob($project, $environment, $request->get('key')));
 
