92cd203cc5627cb331c8de6531f205a40959422a
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-09-22 13:51:18 +1000

    Replaced magic strings with constatns, added deploy cases tp reducer

--- resources/assets/js/state/projectDeployments/reducers.js
+++ resources/assets/js/state/projectDeployments/reducers.js
@@ -1,24 +1,67 @@
-const initialState = {
-  items: [],
-  isFetching: false
-};
+import { initialState } from './state';
+
+import {
+  PROJECT_DEPLOYMENTS_REQUEST,
+  PROJECT_DEPLOYMENTS_SUCCESS,
+  PROJECT_DEPLOYMENTS_FAILURE,
+  PROJECT_DEPLOYMENT_CREATE_REQUEST,
+  PROJECT_DEPLOYMENT_CREATE_SUCCESS,
+  PROJECT_DEPLOYMENT_CREATE_FAILURE,
+  PROJECT_DEPLOYMENT_DEPLOYING,
+  PROJECT_DEPLOYMENT_DEPLOYED,
+} from './constants';
 
 const projectDeployments = (state = initialState, action) => {
   switch(action.type) {
-    case 'PROJECT_DEPLOYMENTS_REQUEST':
+    case PROJECT_DEPLOYMENTS_REQUEST:
       return {
         ...state,
         isFetching: true
       };
-    case 'PROJECT_DEPLOYMENTS_SUCCESS':
+    case PROJECT_DEPLOYMENTS_SUCCESS:
       return {
         items: action.deployments,
         isFetching: false
       };
-    case 'PROJECT_DEPLOYMENTS_FAILURE':
+    case PROJECT_DEPLOYMENTS_FAILURE:
       return {
         ...state,
         isFetching: false
+      };
+    case PROJECT_DEPLOYMENT_CREATE_REQUEST:
+      return {
+        ...state,
+        isCreating: true
+      };
+    case PROJECT_DEPLOYMENT_CREATE_SUCCESS:
+      return {
+        ...state,
+        isCreating: false
+      };
+    case PROJECT_DEPLOYMENT_CREATE_FAILURE:
+      return {
+        ...state,
+        isCreating: false
+      };
+    case PROJECT_DEPLOYMENT_DEPLOYING:
+      return {
+        ...state,
+        items: [action.deployment].concat(state.items.slice(0, 4)),
+      };
+    case PROJECT_DEPLOYMENT_DEPLOYED:
+      const deployments =  state.items.map(deployment => {
+        if (deployment.id === action.deployment.id) {
+          return {
+            ...deployment,
+            status: action.deployment.status,
+          };
+        }
+        return deployment;
+      });
+
+      return {
+        ...state,
+        items: deployments,
       };
 
     default:
