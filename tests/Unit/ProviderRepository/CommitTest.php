41a4cbed6dff4afdd000c8283595586733064fd0
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-06-28 15:51:32 +1000

    Update test names and added test for invalid tag name

--- tests/Unit/ProviderRepository/CommitTest.php
+++ tests/Unit/ProviderRepository/CommitTest.php
@@ -8,7 +8,7 @@ use Deploy\ProviderRepository\Reference;
 
 class CommitTest extends TestCase
 {
-    public function testReturnsCommitHashFromMatchingTag()
+    public function test_returns_commit_hash()
     {
         $tagName = 'v1.0';
         $tags = [
@@ -18,5 +18,14 @@ class CommitTest extends TestCase
         $commit = new Commit(Reference::TAG_TYPE, $tagName);
         
         $this->assertEquals('abc123', $commit->getCommitHashFromtag($tags, $tagName));
+    }
+
+    public function test_tag_does_not_exist_in_list()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $commit = new Commit(Reference::TAG_TYPE, 'v1.0');
+        
+        $commit->getCommitHashFromtag([], 'v1.0');
     }
 }
