52629991f1845633d7cc99994f44eb9e1b4697e0
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-07-05 20:58:09 +1000

    Remove jquery/bootstrap modal usage in project page

---
+++ resources/assets/js/pages/ProjectPage/components/RedeploymentModal.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Modal from '../../../components/Modal';
+
+const RedeploymentModal = (props) => {
+  const {
+    isVisible,
+    onModalHide,
+    redeploy,
+    onRedeploymentClick,
+  } = props;
+
+  const commit = redeploy.commit.substr(0, 7);
+
+  return (
+    <Modal
+      isVisible={ isVisible }
+      id="redeploy-modal"
+      title={`Redeploy Commit (${commit})`}
+      buttons={[
+        {
+          text: 'Cancel', 
+          onPress: () => onModalHide()
+        },
+        {
+          text: 'Redeploy', 
+          onPress: () => onRedeploymentClick()
+        }
+      ]}
+    >
+      Are you sure you want to redeploy this commit?
+    </Modal>
+  )
+}
+
+export default RedeploymentModal;
