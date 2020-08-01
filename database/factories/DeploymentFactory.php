93ded3c727f6dda50fc5f87ed957707c74c3cc92
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-08-01 21:54:12 +1000

    Added faker data

--- database/factories/DeploymentFactory.php
+++ database/factories/DeploymentFactory.php
@@ -3,8 +3,18 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use Deploy\Models\Deployment;
+use Deploy\Models\Project;
 use Faker\Generator;
 
 $factory->define(Deployment::class, function (Generator $faker) {
-    return [];
+    return [
+        'project_id' => factory(Project::class),
+        'committer' => $faker->userName,
+        'committer_avatar' => 'https://path/to/image.jpg',
+        'repository' => 'https://github.com/itsjeffro/deploy.git',
+        'reference' => 'branch',
+        'branch' => 'master',
+        'commit' => $faker->sha1,
+        'commit_url' => 'https://path/to/commit',
+    ];
 });
