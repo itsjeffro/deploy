3efcf77a46ada43f0d316d367d1a3fe7ff7e708f
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-08-01 21:54:26 +1000

    Added faker data

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -3,10 +3,16 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use Deploy\Models\Project;
+use Deploy\Models\User;
 use Faker\Generator;
 
 $factory->define(Project::class, function (Generator $faker) {
     return [
         'name' => $faker->title,
+        'key' => 'ssh-rsa random-public-key',
+        'repository' => 'https://github.com/itsjeffro/deploy.git',
+        'branch' => 'master',
+        'provider_id' => 1,
+        'user_id' => factory(User::class),
     ];
 });
