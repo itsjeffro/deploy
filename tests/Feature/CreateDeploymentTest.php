b0b1c1ee101d5a313c1ec5d3d15481ad70fd1fcc
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-08-01 21:55:05 +1000

    Removed trait and factory override data

--- tests/Feature/CreateDeploymentTest.php
+++ tests/Feature/CreateDeploymentTest.php
@@ -6,21 +6,12 @@ use Deploy\DeploymentManager;
 use Deploy\Models\Project;
 use Deploy\ProviderRepository\Reference;
 use Deploy\Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CreateDeploymentTest extends TestCase
 {
-    use RefreshDatabase;
-
     public function test_successfully_created_deployment()
     {
-        $project = factory(Project::class)->create([
-            'repository' => 'https://github.com/itsjeffro/deploy.git',
-            'branch' => 'master',
-            'provider_id' => 1,
-            'user_id' => 1,
-            'key' => 'ssh-rsa random-public-key',
-        ]);
+        $project = factory(Project::class)->create();
 
         $reference = new Reference(Reference::BRANCH_TYPE, 'master');
 
