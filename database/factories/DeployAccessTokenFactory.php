6428622eadf3b77ed37437b5b24a573c0579cdc8
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-08-08 13:35:55 +1000

    Added access and refresh token factories

---
+++ database/factories/DeployAccessTokenFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Deploy\Models\DeployAccessToken;
+use Deploy\Models\Project;
+use Faker\Generator;
+
+$factory->define(DeployAccessToken::class, function (Generator $faker) {
+    return [];
+});
