01056416458047c8f302641d665d1918814fd3eb
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-11-02 16:59:27 +1100

    Added NotFoundPage component and route

--- resources/assets/js/Application.tsx
+++ resources/assets/js/Application.tsx
@@ -17,6 +17,7 @@ import ProjectDeploymentHookPage from './pages/ProjectDeploymentHookPage';
 import ProjectLinkedFolderPage from './pages/ProjectLinkedFolderPage';
 import AccountPage from './pages/AccountPage';
 import NotificationsPage from './pages/NotificationsPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 
 class Application extends React.Component {
   render() {
@@ -40,6 +41,7 @@ class Application extends React.Component {
             <Route path="/projects/:project_id/settings" component={ ProjectEditPage} />
             <Route path="/projects/:project_id" component={ ProjectPage} />
             <Route path="/account" component={ AccountPage} />
+            <Route component={ NotFoundPage } />
           </Switch>
           <Alert/>
         </>
