04d40072233bcaff578316a131dcb381c4752b17
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-11-02 14:50:49 +1100

    Added wrapping nav__heading

--- resources/assets/js/components/ProjectHeading/ProjectHeading.tsx
+++ resources/assets/js/components/ProjectHeading/ProjectHeading.tsx
@@ -8,20 +8,22 @@ const ProjectHeading = (props) => {
   } = props;
 
   return (
-    <div className="project-heading">
-      <Container fluid>
-        <div className="pull-left">
-          <h2>{project.name}</h2>
-        </div>
+    <div className="project-nav">
+      <div className="project-nav__heading">
+        <Container fluid>
+          <div className="pull-left">
+            <h2>{project.name}</h2>
+          </div>
 
-        <div className="pull-right">
-          { props.children }
-        </div>
+          <div className="pull-right">
+            { props.children }
+          </div>
 
-        <div className="clearfix"></div>
+          <div className="clearfix"></div>
 
-        <ProjectNavigation project={ project } />
-      </Container>
+          <ProjectNavigation project={ project } />
+        </Container>
+      </div>
     </div>
   )
 }
