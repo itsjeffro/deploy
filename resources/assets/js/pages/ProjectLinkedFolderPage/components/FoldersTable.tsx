f98c46a17aa6a8327d2a5645d105727a991f4e4b
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-11-08 19:53:59 +1100

    Perfrm loading in table component

--- resources/assets/js/pages/ProjectLinkedFolderPage/components/FoldersTable.tsx
+++ resources/assets/js/pages/ProjectLinkedFolderPage/components/FoldersTable.tsx
@@ -2,12 +2,22 @@ import * as React from 'react';
 
 import Button from '../../../components/Button';
 import Icon from '../../../components/Icon';
+import PanelBody from '../../../components/PanelBody';
 
-const FoldersTable = (props) => {
+const FoldersTable = (props: any) => {
   const {
+    isLoading,
     folders,
     modalLinkedFolderRemoveShow
   } = props;
+
+  if (isLoading) {
+    return <PanelBody>Loading...</PanelBody>
+  }
+
+  if (folders.length === 0) {
+    return <PanelBody className="text-center">No folders have been added.</PanelBody>
+  }
 
   return (
     <div className="table-responsive">
@@ -20,7 +30,7 @@ const FoldersTable = (props) => {
         </tr>
         </thead>
         <tbody>
-        {folders.map(folder =>
+        {folders.map((folder) => (
           <tr key={folder.id}>
             <td>{folder.from}</td>
             <td>{folder.to}</td>
@@ -31,7 +41,7 @@ const FoldersTable = (props) => {
               ><Icon iconName="times"/></Button>
             </td>
           </tr>
-        )}
+        ))}
         </tbody>
       </table>
     </div>
