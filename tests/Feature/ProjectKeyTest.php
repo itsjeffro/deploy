c5cfb3fed9a93eac4b07e47245642e00ebe753bb
Jeff Nielsen <jefftoddnielsen@gmail.com>
2021-01-09 21:30:06 +1100

    Added project key api test

---
+++ tests/Feature/ProjectKeyTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Deploy\Tests\Feature;
+
+use Deploy\Models\Project;
+use Deploy\Tests\TestCase;
+
+class ProjectKeyTest extends TestCase
+{
+    public function test_owner_can_update_their_projects_key()
+    {
+        $project = factory(Project::class)->create();
+        $user = $project->user;
+
+        $response = $this->actingAs($user)
+            ->json('PUT', route('project-key.update', ['project' => $project->id]));
+
+        $response->assertStatus(200);
+
+        $data = $response->decodeResponseJson();
+
+        $this->assertArrayHasKey('key', $data);
+        $this->assertTrue($data['key'] !== $project->key);
+        $this->assertSame(40, strlen($data['key']));
+    }
+}
