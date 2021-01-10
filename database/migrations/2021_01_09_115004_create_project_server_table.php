104307ceeba1aa72c0d11258375dc91208012e1a
Jeff Nielsen <jefftoddnielsen@gmail.com>
2021-01-10 19:39:46 +1100

    Added initial migrations for project server pivot and update table

---
+++ database/migrations/2021_01_09_115004_create_project_server_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Deploy\Models\Server;
+use Deploy\Models\ProjectServer;
+
+class CreateProjectServerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_server', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('project_id')->unsigned()->nullable();
+            $table->integer('server_id')->unsigned()->nullable();
+
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->foreign('server_id')->references('id')->on('servers');
+        });
+
+        $servers = Server::get();
+
+        foreach ($servers as $server) {
+            ProjectServer::create([
+                'project_id' => $server->project_id,
+                'server_id' => $server->id,
+            ]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('project_server');
+    }
+}
