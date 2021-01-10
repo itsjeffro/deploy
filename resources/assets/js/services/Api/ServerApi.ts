d44a887a5703d88c26a7db8cf0e89a888f146b24
Jeff Nielsen <jefftoddnielsen@gmail.com>
2021-01-10 19:38:46 +1100

    Added api service methods

--- resources/assets/js/services/Api/ServerApi.ts
+++ resources/assets/js/services/Api/ServerApi.ts
@@ -7,8 +7,29 @@ class ServerApi extends BaseApi {
    */
   list(options) {
     const page = options.page || 1;
-
+  
     return this.getRequest(`/api/servers?page=${page}`);
+  }
+  
+  /**
+   * Get server by id.
+   */
+  get(serverId: number): any {
+    return this.getRequest(`/api/servers/${ serverId }`);
+  }
+  
+  /**
+   * Update server by id.
+   */
+  update(serverId: number, data: any): any {
+    return this.putRequest(`/api/servers/${ serverId }`, data);
+  }
+  
+  /**
+   * Delete server by id.
+   */
+  delete(serverId: number): any {
+    return this.deleteRequest(`/api/servers/${ serverId }`);
   }
 }
 
