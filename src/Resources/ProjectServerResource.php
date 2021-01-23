33fa5a6ea9b178e0a2280e1f0d9485eba55c64b9
jeff.nielsen <jeff.n@covergenius.com>
2021-01-23 19:38:40 +1100

    Added initial project server api resource

---
+++ src/Resources/ProjectServerResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Deploy\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectServerResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->server_id,
+            'project_id' => $this->project_id,
+            'name' => $this->server->name,
+            'connect_as' => $this->server->connect_as,
+            'ip_address' => $this->server->ip_address,
+            'port' => $this->server->port,
+            'project_path' => $this->server->project_path,
+            'public_key' => $this->server->public_key,
+            'connection_status' => $this->server->connection_status,
+        ];
+    }
+}
