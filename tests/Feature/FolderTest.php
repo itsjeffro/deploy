47ec1f49cf7d96e7c99c3f165e252eda151852ee
jeff.nielsen <jeff.n@covergenius.com>
2021-01-25 08:55:10 +1100

    Added list and delete test cases

--- tests/Feature/FolderTest.php
+++ tests/Feature/FolderTest.php
@@ -2,11 +2,28 @@
 
 namespace Deploy\Tests\Feature;
 
+use Deploy\Models\Folder;
 use Deploy\Models\Project;
 use Deploy\Tests\TestCase;
 
 class FolderTest extends TestCase
 {
+    public function test_successfully_list_folders()
+    {
+        $project = factory(Project::class)->create();
+        $user = $project->user;
+        $project->folders()->createMany(
+            factory(Folder::class, 1)->make()->toArray()
+        );
+
+        $route = route('project-folders.index', $project);
+
+        $response = $this->actingAs($user)
+            ->json('GET', $route);
+
+        $response->assertStatus(200);
+    }
+
     public function test_successfully_create_folder()
     {
         $project = factory(Project::class)->create();
@@ -26,5 +43,24 @@ class FolderTest extends TestCase
             'from' => 'uploads',
             'to' => 'storage/uploads',
         ]);
+    }
+
+    public function test_successfully_delete_folder()
+    {
+        $project = factory(Project::class)->create();
+        $folder = factory(Folder::class)->create([
+            'project_id' => $project->id,
+        ]);
+        $user = $project->user;
+
+        $route = route('project-folders.destroy', [
+            'project' => $project->id,
+            'folder' => $folder->id
+        ]);
+
+        $response = $this->actingAs($user)
+            ->json('DELETE', $route);
+
+        $response->assertStatus(204);
     }
 }
