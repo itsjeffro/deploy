098f5a8c86b82319ebf0c04883d3f0ec010b7234
jeff.nielsen <jeff.n@covergenius.com>
2021-01-24 21:04:40 +1100

    Use servers relationship

--- src/Resources/ProjectResource.php
+++ src/Resources/ProjectResource.php
@@ -28,7 +28,7 @@ class ProjectResource extends JsonResource
             'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
             'environment_servers' => $this->environmentServers,
             'user' => $this->user,
-            'servers' => ProjectServerResource::collection($this->servers),
+            'servers' => $this->servers,
             'folders' => $this->folders,
             'last_deployment' => $this->lastDeployment,
             'provider' => $this->provider,
