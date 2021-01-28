418434007e4ccf477b2f8eebd31ad9af5fbfe895
jeff.nielsen <jeff.n@covergenius.com>
2021-01-29 08:53:05 +1100

    Added update and view to ProjectServerPolicy

--- src/Policies/ProjectServerPolicy.php
+++ src/Policies/ProjectServerPolicy.php
@@ -22,6 +22,22 @@ class ProjectServerPolicy
     }
 
     /**
+     * Show project server belonging to user.
+     */
+    public function view($user, ProjectServer $projectServer): bool
+    {
+        return $user->id === $projectServer->project->user_id;
+    }
+
+    /**
+     * Update project server belonging to user.
+     */
+    public function update($user, ProjectServer $projectServer): bool
+    {
+        return $user->id === $projectServer->project->user_id;
+    }
+
+    /**
      * Delete project belonging to user.
      */
     public function delete($user, ProjectServer $projectServer): bool
