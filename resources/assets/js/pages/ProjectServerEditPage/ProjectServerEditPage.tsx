c66895b341d698f441b752f1dc00d031889942a4
jeff.nielsen <jeff.n@covergenius.com>
2021-01-30 10:06:37 +1100

    Handle server update and change

--- resources/assets/js/pages/ProjectServerEditPage/ProjectServerEditPage.tsx
+++ resources/assets/js/pages/ProjectServerEditPage/ProjectServerEditPage.tsx
@@ -4,7 +4,7 @@ import { Redirect } from 'react-router-dom';
 import Select from 'react-select'
 
 import { createToast } from '../../state/alert/alertActions';
-import {getServer, listServers} from "../../state/servers/actions";
+import { listServers } from "../../state/servers/actions";
 import AlertErrorValidation from '../../components/AlertErrorValidation';
 import Button from '../../components/Button';
 import Panel from '../../components/Panel';
@@ -13,23 +13,38 @@ import Container from '../../components/Container';
 import PanelHeading from '../../components/PanelHeading';
 import PanelTitle from '../../components/PanelTitle';
 import ProjectHeading from "../../components/ProjectHeading";
-import {fetchProject, updateProjectServer} from "../../state/project/actions";
 import TextField from "../../components/TextField";
 import PanelBody from "../../components/PanelBody";
 import Grid from "../../components/Grid";
+import Project from "../../services/Project";
+import ProjectServerApi from "../../services/Api/ProjectServerApi";
 
 class ProjectServerEditPage extends React.Component<any, any> {
   state = {
     isFetching: true,
-    isCreated: false,
-    serverId: null,
-    input: {
+    isUpdated: false,
+    isUpdating: false,
+    errors: [],
+    initialServer: null,
+    server: {
+      id: null,
       name: '',
       ip_address: '',
       port: '',
       connect_as: '',
+    },
+    project: {
+      id: null,
+      servers: [],
+    },
+    input: {
+      server_id: '',
       project_path: '',
     },
+    selectedServer: {
+      value: '',
+      label: '',
+    }
   };
 
   componentDidMount() {
@@ -43,22 +58,40 @@ class ProjectServerEditPage extends React.Component<any, any> {
       },
     } = this.props;
 
-    dispatch(getServer(server_id));
+    const projectApi = new Project();
 
-    dispatch(fetchProject(project_id));
+    projectApi.get(project_id)
+      .then((response) => this.setState({ project: response.data }))
+      .catch((error) => console.log(error.response));
+
+    const projectServerApi = new ProjectServerApi();
+
+    projectServerApi.get(project_id, server_id)
+      .then((response) => {
+        const projectServer = response.data;
+
+        this.setState({
+          initialServer: projectServer.server.id,
+          input: {
+            server_id: projectServer.server.id,
+            project_path: projectServer.project_path,
+          },
+          selectedServer: {
+            value: projectServer.server.id,
+            label: projectServer.server.name,
+          },
+          server: {
+            id: projectServer.server.id,
+            name: projectServer.server.name,
+            ip_address: projectServer.server.ip_address,
+            port: projectServer.server.port,
+            connect_as: projectServer.server.connect_as,
+          }
+        })
+      })
+      .catch((error) => console.log(error.response));
 
     dispatch(listServers());
-  }
-
-  componentWillReceiveProps(nextProps: any) {
-    const { dispatch, servers } = this.props;
-
-    // Handler project server create
-    if (nextProps.servers.isCreated !== servers.isCreated && nextProps.servers.isCreated) {
-      dispatch(createToast('Server updated successfully.'));
-      
-      this.setState({ isCreated: true });
-    }
   }
 
   /**
@@ -82,37 +115,83 @@ class ProjectServerEditPage extends React.Component<any, any> {
    * Handle click for creating a server.
    */
   handleSaveServerClick = (): void => {
-    const { dispatch, project } = this.props;
-    const { input } = this.state;
+    const { dispatch } = this.props;
+    const { input, project, initialServer } = this.state;
 
-    dispatch(updateProjectServer(project.item.id, 0, input));
+    const projectServerApi = new ProjectServerApi();
+
+    this.setState({ isUpdating: true });
+
+    projectServerApi.update(project.id, initialServer, input)
+      .then((response) => {
+        this.setState({ isUpdated: true, isUpdating: false });
+
+        dispatch(createToast('Server updated successfully.'));
+      })
+      .catch((error) => {
+        this.setState({ errors: error.response, isUpdating: false });
+      });
   };
 
   /**
    * Handle existing server option select change.
    */
   handleServerChange = (option): void => {
-    let serverId = option ? option.value : null;
+    const { servers } = this.props;
+    const serverId = option ? option.value : null;
+    const server = this.getServerFromList(servers.items, serverId);
 
     this.setState((prevState) => {
-      const input = {
-        ...prevState.input,
-        server_id: serverId,
+      const selectedServer = {
+        ...prevState.selectedServer,
+        label: server.name,
+        value: server.id,
       };
 
-      return { input: input };
+      return {
+        selectedServer: selectedServer,
+        server: {
+          id: server.id,
+          name: server.name,
+          ip_address: server.ip_address,
+          port: server.port,
+          connect_as: server.connect_as,
+        },
+        input: {
+          ...prevState.input,
+          server_id: server.id,
+          project_path: server.project_path,
+        },
+      };
     });
+  }
+
+  /**
+   * Return selected server.
+   */
+  protected getServerFromList(servers: any[], serverId: number): any {
+    const filteredServers = servers.filter((server) => {
+      return server.id == serverId;
+    });
+
+    if (filteredServers.length === 0) {
+      const availableIds = servers.map((server) => server.id).join(', ');
+
+      throw new Error(`Server [${ serverId }] does not exist. Available Ids: ${ availableIds } }`);
+    }
+
+    return filteredServers[0]
   }
 
   /**
    * Render component.
    */
   render() {
-    const { project, servers } = this.props;
-    const { isCreated } = this.state;
+    const { servers } = this.props;
+    const { project, server, isUpdated, isUpdating, errors, input, selectedServer } = this.state;
 
-    if (isCreated) {
-      return <Redirect to={ `/projects/${ project.item.id }` } />
+    if (isUpdated) {
+      return <Redirect to={ `/projects/${ project.id }` } />
     }
 
     const serverOptions = servers.items.map((server: any) => ({
@@ -122,7 +201,7 @@ class ProjectServerEditPage extends React.Component<any, any> {
 
     return (
       <Layout>
-        <ProjectHeading project={ project.item } />
+        <ProjectHeading project={ project } />
 
         <div className="content">
           <Container fluid>
@@ -132,14 +211,14 @@ class ProjectServerEditPage extends React.Component<any, any> {
               </PanelHeading>
 
               <PanelBody>
-                { servers.errors.length ? <AlertErrorValidation errors={ servers.errors } /> : '' }
+                { errors.length ? <AlertErrorValidation errors={ errors } /> : '' }
 
                 <div className="form-group">
                   <label htmlFor="name">Server</label>
                   <Select
                     options={ serverOptions }
                     onChange={ (option) => this.handleServerChange(option) }
-                    value={ { label: servers.item.name, value: servers.item.id } }
+                    value={ { label: selectedServer.label, value: selectedServer.value } }
                   />
                 </div>
 
@@ -147,13 +226,13 @@ class ProjectServerEditPage extends React.Component<any, any> {
                   <Grid md={ 6 }>
                     <div className="form-group">
                       <label>Ip address</label>
-                      <div>{ servers.item.ip_address }</div>
+                      <div>{ server.ip_address }</div>
                     </div>
                   </Grid>
                   <Grid md={ 6 }>
                     <div className="form-group">
                       <label>Port</label>
-                      <div>{ servers.item.port }</div>
+                      <div>{ server.port }</div>
                     </div>
                   </Grid>
                 </div>
@@ -162,7 +241,7 @@ class ProjectServerEditPage extends React.Component<any, any> {
                   <Grid md={ 12 }>
                     <div className="form-group">
                       <label>Connect as</label>
-                      <div>{ servers.item.connect_as }</div>
+                      <div>{ server.connect_as }</div>
                     </div>
                   </Grid>
                 </div>
@@ -172,13 +251,13 @@ class ProjectServerEditPage extends React.Component<any, any> {
                   id="project_path"
                   name="project_path"
                   onChange={ this.handleInputChange }
-                  value={ servers.item.project_path }
+                  value={ input.project_path }
                 />
 
                 <Button
                   color="primary"
                   onClick={ this.handleSaveServerClick }
-                >{ servers.isCreating ? 'Working...' : 'Update Server' }</Button>
+                >{ isUpdating ? 'Working...' : 'Update Server' }</Button>
               </PanelBody>
             </Panel>
           </Container>
@@ -190,7 +269,6 @@ class ProjectServerEditPage extends React.Component<any, any> {
 
 const mapStateToProps = (state) => {
   return {
-    project: state.project,
     servers: state.servers,
   };
 };
