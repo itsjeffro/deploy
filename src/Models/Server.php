461689fd4528a9c118cb5efdb96e25fe41945841
jeff.nielsen <jeff.n@covergenius.com>
2021-01-30 13:47:39 +1100

    Added import

--- src/Models/Server.php
+++ src/Models/Server.php
@@ -5,6 +5,7 @@ namespace Deploy\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
