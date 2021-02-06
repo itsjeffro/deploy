23a08fa9ce823ab946970a8f3ca258a52d1d0aa6
jeff.nielsen <jeff.n@covergenius.com>
2021-02-06 15:21:42 +1100

    Removed project_id param

--- resources/assets/js/services/Api/ServerTestConnectionApi.ts
+++ resources/assets/js/services/Api/ServerTestConnectionApi.ts
@@ -2,8 +2,8 @@ import '../../bootstrap';
 import BaseApi from './BaseApi';
 
 class ServerTestConnectionApi extends BaseApi {
-  get(project_id, server_id) {
-    return this.getRequest('/api/servers/' + server_id + '/test-connection');
+  get(server_id: number) {
+    return this.getRequest(`/api/servers/${ server_id }/test-connection`);
   }
 }
 
