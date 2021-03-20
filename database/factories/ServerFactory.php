8564276030903b0c5470a73c13a4a42cd0369380
jeff.nielsen <jeff.n@covergenius.com>
2021-03-20 23:06:58 +1100

    Removed redundant fields

--- database/factories/ServerFactory.php
+++ database/factories/ServerFactory.php
@@ -9,13 +9,11 @@ use Faker\Generator;
 
 $factory->define(Server::class, function (Generator $faker) {
     return [
-        'project_id' => factory(Project::class),
         'user_id' => factory(User::class),
         'name' => 'Server name',
         'port' => 3306,
         'ip_address' => '127.0.0.1',
         'connect_as' => 'root',
-        'project_path' => '/var/www/html',
         'public_key' => 'ssh-rsa random-key',
     ];
 });
