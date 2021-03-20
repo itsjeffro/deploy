3411018ba6fb23717322f7f9f9bf6ae6479f5f14
jeff.nielsen <jeff.n@covergenius.com>
2021-03-20 18:23:30 +1100

    Removed redundant imports

--- database/factories/FolderFactory.php
+++ database/factories/FolderFactory.php
@@ -3,9 +3,7 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use Deploy\Models\Folder;
-use Deploy\Models\User;
 use Faker\Generator;
-use Illuminate\Support\Str;
 
 $factory->define(Folder::class, function (Generator $faker) {
     return [
