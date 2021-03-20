5301b380ed0511ea7be6b277a6f7225415f53821
jeff.nielsen <jeff.n@covergenius.com>
2021-03-20 17:44:10 +1100

    Added missing throw to docblock

--- src/Http/Controllers/ServerTestConnectionController.php
+++ src/Http/Controllers/ServerTestConnectionController.php
@@ -4,12 +4,15 @@ namespace Deploy\Http\Controllers;
 
 use Deploy\Jobs\TestConnectionJob;
 use Deploy\Models\Server;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\JsonResponse;
 
 class ServerTestConnectionController extends Controller
 {
     /**
      * Get server connection status.
+     *
+     * @throws AuthorizationException
      */
     public function show(Server $server): JsonResponse
     {
