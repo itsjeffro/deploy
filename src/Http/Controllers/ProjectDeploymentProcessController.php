c040121a29b9bbc953d150a1d56e073f8be2b760
jeff.nielsen <jeff.n@covergenius.com>
2021-03-20 23:08:07 +1100

    Removed redundant docblock params

--- src/Http/Controllers/ProjectDeploymentProcessController.php
+++ src/Http/Controllers/ProjectDeploymentProcessController.php
@@ -5,18 +5,14 @@ namespace Deploy\Http\Controllers;
 use Deploy\Models\Deployment;
 use Deploy\Models\Process;
 use Deploy\Models\Project;
+use Illuminate\Http\JsonResponse;
 
 class ProjectDeploymentProcessController extends Controller
 {
     /**
      * Show process output.
-     *
-     * @param  \Deploy\Models\Project $project
-     * @param  \Deploy\Models\Deployment $deployment
-     * @param  \Deploy\Models\Process $process
-     * @return \Illuminate\Http\JsonResponse
      */
-    public function show(Project $project, Deployment $deployment, Process $process)
+    public function show(Project $project, Deployment $deployment, Process $process): JsonResponse
     {
         if ($project->id !== $deployment->project_id || $deployment->id !== $process->deployment_id) {
             abort(404, 'Not found.');
