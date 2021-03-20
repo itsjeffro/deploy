1f72c9717ac55c289052b23162f39eac3f1aa5b6
jeff.nielsen <jeff.n@covergenius.com>
2021-03-20 15:51:57 +1100

    Removed type on User arg

--- src/Policies/DeploymentPolicy.php
+++ src/Policies/DeploymentPolicy.php
@@ -2,32 +2,21 @@
 
 namespace Deploy\Policies;
 
-use App\User;
 use Deploy\Models\Deployment;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class DeploymentPolicy
 {
     use HandlesAuthorization;
-
-    /**
-     * Create a new policy instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
     
     /**
      * Show deployment belonging to user.
      *
-     * @param \App\User $user
-     * @param \Deploy\Models\Deployment $deployment
+     * @param $user
+     * @param Deployment $deployment
      * @return bool
      */
-    public function view(User $user, Deployment $deployment)
+    public function view($user, Deployment $deployment): bool
     {
         return $user->id === $deployment->project->user_id;
     }
