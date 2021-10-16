157d7717992349b74b4520c2b0e166e89eadff44
jeff.nielsen <jeff.n@covergenius.com>
2021-10-16 17:40:17 +1100

    Fixed tests

--- tests/Integration/BitbucketOauthTest.php
+++ tests/Integration/BitbucketOauthTest.php
@@ -2,83 +2,98 @@
 
 namespace Deploy\Tests\Integration;
 
-use Deploy\Contracts\ProviderOauth\ProviderOauthResourceInterface;
 use Deploy\Models\DeployAccessToken;
 use Deploy\Models\DeployRefreshToken;
+use Deploy\Models\Provider;
 use Deploy\Models\User;
 use Deploy\ProviderOauth\Bitbucket\BitbucketOauth;
+use Deploy\ProviderOauth\Bitbucket\BitbucketOauthResource;
 use Deploy\ProviderOauthManager;
 use Deploy\Tests\TestCase;
 use Exception;
+use Illuminate\Support\Carbon;
 
 class BitbucketOauthTest extends TestCase
 {
+    /**
+     * @var int
+     */
     const TWO_HOURS_IN_SECONDS = 7200;
 
+    /**
+     * @group oauth
+     */
     public function test_get_non_existent_token_throws_exception()
     {
         $this->expectException(Exception::class);
 
-        $provider = \Mockery::mock(BitbucketOauth::class)->makePartial();
         $user = User::factory()->create();
 
+        $bitbucketProvider = \Mockery::mock(BitbucketOauth::class)->makePartial();
+
         $providerOauthManager = new ProviderOauthManager;
-        $providerOauthManager->setProvider($provider);
+        $providerOauthManager->setProvider($bitbucketProvider);
         $providerOauthManager->setUser($user);
 
         $providerOauthManager->getAccessToken();
     }
 
+    /**
+     * @group oauth
+     */
     public function test_successfully_request_new_token()
     {
-        $token = $this->mockProviderOauthWithAccessToken('bitbucket_requested_access_token_value');
-
-        $provider = \Mockery::mock(BitbucketOauth::class)->makePartial();
-        $provider->shouldReceive('requestAccessToken')->andReturn($token);
-        $provider->shouldReceive('refreshAccessToken')->andReturn($token);
-
         $user = User::factory()->create();
 
+        $this->createAccessToken($user);
+
+        $newAccessToken = new BitbucketOauthResource([
+            'access_token' => '::NEW_ACCESS_TOKEN::',
+            'refresh_token' => '::NEW_REFRESH_TOKEN::',
+            'token_type' => 'bearer',
+            'scopes' => 'project',
+            'expires_in' => time() + self::TWO_HOURS_IN_SECONDS,
+        ]);
+
+        $bitbucketProvider = \Mockery::mock(BitbucketOauth::class)->makePartial();
+        $bitbucketProvider->shouldReceive('requestAccessToken')->andReturn($newAccessToken);
+        $bitbucketProvider->shouldReceive('refreshAccessToken')->andReturn($newAccessToken);
+
         $providerOauthManager = new ProviderOauthManager;
-        $providerOauthManager->setProvider($provider);
+        $providerOauthManager->setProvider($bitbucketProvider);
         $providerOauthManager->setUser($user);
 
-        $requestedToken = $providerOauthManager->requestAccessToken('authorization_code');
+        $requestedToken = $providerOauthManager->requestAccessToken('::BITBUCKET_AUTH_CODE::');
         $storedToken = $providerOauthManager->getAccessToken();
 
         $this->assertSame($requestedToken, $storedToken);
     }
 
+    /**
+     * @group oauth
+     */
     public function test_expired_token_returns_refreshed_token()
     {
-        $expirationDate = new \DateTime();
-        $expirationDate->sub(new \DateInterval('P1D'));
-
-        $token = $this->mockProviderOauthWithAccessToken('bitbucket_new_access_token_value');
-
-        $provider = \Mockery::mock(BitbucketOauth::class)->makePartial();
-        $provider->shouldReceive('requestAccessToken')->andReturn($token);
-        $provider->shouldReceive('refreshAccessToken')->andReturn($token);
-
         $user = User::factory()->create();
 
-        $oldAccessToken = DeployAccessToken::factory()->create([
-            'id' => 'bitbucket_old_access_token_value',
-            'provider_id' => 1,
-            'user_id' => $user,
-            'scopes' => 'account webhook project',
-            'expires_at' => $expirationDate,
+        $newAccessToken = new BitbucketOauthResource([
+            'access_token' => '::NEW_ACCESS_TOKEN::',
+            'refresh_token' => '::NEW_REFRESH_TOKEN::',
+            'token_type' => 'bearer',
+            'scopes' => 'project',
+            'expires_in' => time(),
         ]);
 
-        $refreshToken = DeployRefreshToken::factory()->create([
-            'id' => 'bitbucket_refresh_token_value',
-            'deploy_access_token_id' => 'bitbucket_old_access_token_value',
-            'revoked' => 0,
-            'expires_at' => $expirationDate,
-        ]);
+        $bitbucketProvider = \Mockery::mock(BitbucketOauth::class)->makePartial();
+        $bitbucketProvider->shouldReceive('requestAccessToken')->andReturn($newAccessToken);
+        $bitbucketProvider->shouldReceive('refreshAccessToken')->andReturn($newAccessToken);
 
+        // Set up data
+        $oldAccessToken = $this->createAccessToken($user);
+
+        // Actual test
         $providerOauthManager = new ProviderOauthManager;
-        $providerOauthManager->setProvider($provider);
+        $providerOauthManager->setProvider($bitbucketProvider);
         $providerOauthManager->setUser($user);
 
         $refreshedToken = $providerOauthManager->getAccessToken();
@@ -86,27 +101,39 @@ class BitbucketOauthTest extends TestCase
         $this->assertNotSame($oldAccessToken->id, $refreshedToken);
     }
 
-    public function test_get_authorizatoin_url()
+    /**
+     * @group oauth
+     */
+    public function test_get_authorisation_url()
     {
-        $provider = \Mockery::mock(BitbucketOauth::class)->makePartial();
         $user = User::factory()->create();
 
+        $bitbucketProvider = \Mockery::mock(BitbucketOauth::class)->makePartial();
+
         $providerOauthManager = new ProviderOauthManager;
-        $providerOauthManager->setProvider($provider);
+        $providerOauthManager->setProvider($bitbucketProvider);
         $providerOauthManager->setUser($user);
 
         $this->assertNotEmpty($providerOauthManager->getAuthorizeUrl());
     }
 
-    private function mockProviderOauthWithAccessToken(string $accessTokenId)
+    private function createAccessToken(User $user): DeployAccessToken
     {
-        $token = \Mockery::mock(ProviderOauthResourceInterface::class);
-        $token->shouldReceive('getAccessToken')->andReturn($accessTokenId);
-        $token->shouldReceive('getRefreshToken')->andReturn('refresh_token_value');
-        $token->shouldReceive('getScopes')->andReturn('account webhook project');
-        $token->shouldReceive('getTokenType')->andReturn('bearer');
-        $token->shouldReceive('getExpiration')->andReturn(self::TWO_HOURS_IN_SECONDS);
+        $provider = Provider::where('friendly_name', Provider::BITBUCKET_PROVIDER)->firstOrFail();
 
-        return $token;
+        $accessToken = DeployAccessToken::factory()->create([
+            'id' => '::OLD_ACCESS_TOKEN::',
+            'user_id' => $user->getKey(),
+            'provider_id' => $provider->getKey(),
+            'expires_at' => Carbon::now()->subDay(),
+        ]);
+
+        DeployRefreshToken::factory()->create([
+            'id' => '::REFRESH_TOKEN::',
+            'deploy_access_token_id' => $accessToken->getKey(),
+            'expires_at' => Carbon::now()->addDay(),
+        ]);
+
+        return $accessToken;
     }
 }
