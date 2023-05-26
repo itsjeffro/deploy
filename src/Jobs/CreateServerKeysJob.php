fb13aa904cc78efb5062781284b91c7a35809b61
Jeff Nielsen <jefftoddnielsen@gmail.com>
2023-05-26 14:57:04 +1000

    Retrieve private key from getter method

--- src/Jobs/CreateServerKeysJob.php
+++ src/Jobs/CreateServerKeysJob.php
@@ -4,6 +4,7 @@ namespace Deploy\Jobs;
 
 use Deploy\Events\ProcessorErrorEvent;
 use Deploy\Models\Server;
+use Deploy\Ssh\Key;
 use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -16,24 +17,8 @@ class CreateServerKeysJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    /** @var Server */
-    private $server;
-    
-    /** @var array */
-    private $sshKeys;
-
-    /**
-     * Create a new job instance.
-     *
-     * @param Server $server
-     * @param array $sshKeys
-     * @return void
-     */
-    public function __construct(Server $server, array $sshKeys)
-    {
-        $this->server = $server;
-        $this->sshKeys = $sshKeys;
-    }
+    public function __construct(private Server $server, private Key $rsa)
+    {}
 
     /**
      * Execute the job.
@@ -49,7 +34,7 @@ class CreateServerKeysJob implements ShouldQueue
 
             $filePath = sprintf('%s/%s', $keysDirectory, $this->server->id);
 
-            Storage::put($filePath, $this->sshKeys['privatekey']);
+            Storage::put($filePath, $this->rsa->getPrivateKey());
 
             $fileExists = Storage::exists($filePath);
 
